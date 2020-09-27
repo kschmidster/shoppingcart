ba31b2986c893baca75d2e50f2e656ed57e50ffe
kschmidster <kschmidster@gmail.com>
2020-09-27 14:50:06 +0200

    initial commit

---
+++ src/test/java/ch/kschmidster/shoppinglist/ShoppinglistApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.kschmidster.shoppinglist;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppinglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+//		assertFalse(true);
+	}
+
+}
